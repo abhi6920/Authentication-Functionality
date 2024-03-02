ae3ebb4bbf5151c03bb0cdff181d676d3c69389c
Abhinay Ponaganti <ponagantiabhinay@gmail.com>
2024-03-02 09:49:27 +0000

    Functionality Project

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+// Write your JS code here
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Header = () => (
+  <li className="header">
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+  </li>
+)
+
+export default Header
